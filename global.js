cc0f28dae78b13152394296bb10942ea7263176e
emong1661-star <emong1661@gmail.com>
2026-04-16 19:00:29 +0900

    Update global.js

--- global.js
+++ global.js
@@ -11,8 +11,8 @@
 
   // --- Domain Restriction ---
   const ALLOWED_HOSTNAMES = [
-    "ejdpeltus4.imweb.me",
-    "xn--2-9z8ey8b4wynnf.shop",
+    "qmfforfkqps1.imweb.me",
+    "xn--oi2b4b88sssb.com",
     "localhost",
     "127.0.0.1",
     "black-label2.netlify.app",
